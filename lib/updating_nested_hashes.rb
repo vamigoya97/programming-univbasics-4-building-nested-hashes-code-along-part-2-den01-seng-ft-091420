226b2e4c861dc240418f074850be2ab45975bf9c
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-07 22:12:39 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -40,8 +40,8 @@ def add_characters
             {name: "Benvolio", age: "17", attitude: "worried"},
             {name: "Mercutio", age: "18", attitude: "hot-headed"}
          ]
-      },
-      {:capulet => {
+      }
+      :capulet => {
          :patriarch => {name: "Lord Capulet", age: "50"},
          :matriarch => {name: "Lady Capulet", age: "51"},
          :heroine => {name: "Juliet", age: "15", status: "alive"},
